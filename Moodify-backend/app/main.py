e8c9f386e9372ffc7a8aa5cae1b34e0508ffba05
Rajib Kumar Maharana <rajibmaharana8200@gmail.com>
2026-01-02 04:43:14 +0530

    Emotion prediction Fast API

--- Moodify-backend/app/main.py
+++ Moodify-backend/app/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from app.emotion_model import predict_emotion
 
 app = FastAPI(title="Moodify Backend API")
 
 @app.get("/")
 def root():
-    return {"message": "Moodify backend is running successfully"}
-    
+    return {"message": "Moodify backend running"}
+
+@app.post("/predict-emotion")
+async def predict_emotion_api(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    emotion = predict_emotion(image_bytes)
+    return {
+        "emotion": emotion
+    }
